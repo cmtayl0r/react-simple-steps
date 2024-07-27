ea4eec7ef24a40668134f77c53609ca792a17704
Chris Taylor <134958907+cmtayl0r@users.noreply.github.com>
2024-07-27 12:34:58 +0200

    add basic event functions for next and previous

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 function App() {
+  const step = 1;
+
+  const handlePrevious = () => {
+    // handle previous click
+  };
+  const handleNext = () => {
+    // handle next click
+  };
+
   return (
-    <div>
-      <h1>React Simple Steps</h1>
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#FFF" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#FFF" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
